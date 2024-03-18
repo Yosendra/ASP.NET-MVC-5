97c738cdac73f7061ff788ce1e77a407af049211
Yosendra <yosendra.evri@gmail.com>
2024-03-19 01:50:48 +0800

    ASP.NET MVC Fundamentals - 05. Passing Data to Views

---
+++ 02. ASP.NET MVC Fundamentals/05. Passing Data to Views/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Random()
+        {
+            var movie = new Movie
+            {
+                Name = "Shrek",
+            };
+
+            // passing movie through ViewData
+            ViewData["Movie"] = movie;
+
+            // passing movie through ViewBag
+            //ViewBag.Movie = movie;
+
+            // passing movie through View() method
+            //return View(movie);
+
+            return View();
+        }
+    }
+}
---
+++ 02. ASP.NET MVC Fundamentals/05. Passing Data to Views/Vidly/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* Passing Data to Views
+ * 
+ * Before, we pass our model to the view by passing it through as an argument to the View() method
+ * There are other two ways to pass data to the views
+ * • ViewData
+ * • ViewBag 
+ * 
+ * Look at:
+ * • MoviesController.cs
+ * • Random.cshtml
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
