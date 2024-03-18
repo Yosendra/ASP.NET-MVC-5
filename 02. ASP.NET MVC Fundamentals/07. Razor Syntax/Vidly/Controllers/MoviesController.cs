57b3d790019b4fc66755300d515709c4b23fb891
Yosendra <yosendra.evri@gmail.com>
2024-03-19 02:34:26 +0800

    ASP.NET MVC Fundamentals - 07. Razor Syntax

---
+++ 02. ASP.NET MVC Fundamentals/07. Razor Syntax/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Random()
+        {
+            var movie = new Movie
+            {
+                Name = "Shrek",
+            };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" },
+                new Customer { Name = "Customer 3" },
+                new Customer { Name = "Customer 4" },
+                new Customer { Name = "Customer 5" },
+                new Customer { Name = "Customer 6" },
+            };
+
+            var viewModel = new RandomMovieVM
+            {
+                Movie = movie,
+                Customers = customers,
+            };
+
+            return View(viewModel);
+        }
+    }
+}
