1f96718aab7b6819e51a62bffc94a4c786f0b2e0
Yosendra <yosendra.evri@gmail.com>
2024-03-19 02:13:58 +0800

    ASP.NET MVC Fundamentals - 06. View Models

---
+++ 02. ASP.NET MVC Fundamentals/06. View Models/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Random()
+        {
+            var movie = new Movie
+            {
+                Name = "Shrek",
+            };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" },
+            };
+
+            var viewModel = new RandomMovieVM
+            {
+                Movie = movie,
+                Customers = customers,
+            };
+
+            // pass the view model instead
+            return View(viewModel);
+        }
+    }
+}
---
+++ 02. ASP.NET MVC Fundamentals/06. View Models/Vidly/Global.asax.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* View Models
+ * 
+ * If we want to pass two or more unrelated models to the view. For example we want to pass
+ * A movie with its customer, we can use ViewModel
+ * 
+ * ViewModel is a model specifically built for a view
+ * It includes any data and rules specific to that view
+ * 
+ * Add Customer class under Models folder
+ * 
+ * We use Models folder purely for domain classes like Movie, Customer, etc
+ * We put ViewModel in a seperete folder
+ * Create a folder named ViewModels
+ * Create ViewModel class under that folder named "RandomMovieVM"
+ * 
+ * In MoviesController, in Random() action, construct the ViewModel class
+ * and pass it to the View() method
+ * 
+ * In Random.cshtml, bind the model type to RandomMovieVM instead of Movie
+ * 
+ * Look at:
+ * • Customer.cs
+ * • RandomMovieVM.cs
+ * • MoviesController.cs -> Random()
+ * • Random.cshtml
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
