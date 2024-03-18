c42933774dbe45479068c1060f55a3987efbbb82
Yosendra <yosendra.evri@gmail.com>
2024-03-18 23:09:49 +0800

    ASP.NET MVC Fundamentals - 02. Action Parameters

---
+++ 02. ASP.NET MVC Fundamentals/02. Action Parameters/Vidly/Controllers/MovieController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        public ActionResult Random()
+        {
+            var movie = new Movie
+            {
+                Name = "Shrek",
+            };
+
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        /* to make the parameter optional we need to make it nullable
+         * int? in this case for page index, string is already nullable for sortBy
+        */
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            string message = string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy);
+            return Content(message);
+        }
+    }
+}
---
+++ 02. ASP.NET MVC Fundamentals/02. Action Parameters/Vidly/Global.asax.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* Action Parameters
+ * 
+ * Before, ActionResult is the output for action method.
+ * Now, action parameters is the input for the action.
+ * 
+ * Parameter Binding
+ * Request => MVC Framework => Action
+ * 
+ * Parameter Source
+ * • In the url:            /movies/edit/[1]
+ * • In the query string:   /movies/edit?[id=1]
+ * • In the form data:      id=1
+ * 
+ * Test : /movie/edit/4
+ *        /movie/edit?id=4
+ * 
+ * Create Index() action method in MovieController, implement default parameter value
+ * 
+ * Test : /movie
+ *        /movie?pageIndex=12&sortBy=Age
+ *        
+ * Right now we cannot embed these two parameters pageIndex & sortBy in the url
+ * It will be solved in custom route
+ * 
+ * Look at:
+ * • MovieController => Edit(), Index()
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
