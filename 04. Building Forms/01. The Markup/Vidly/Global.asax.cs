c7a08a61d0323c99888f8d97af039c1043761eee
Yosendra <yosendra.evri@gmail.com>
2024-03-19 16:40:45 +0800

    Building Forms - 01. The Markup

---
+++ 04. Building Forms/01. The Markup/Vidly/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* The Markup
+ * 
+ * We are going to create a form for adding customer
+ * Create action method for opening add customer in CustomersController
+ * 
+ * Look at:
+ * • CustomersController.cs -> New(), New.cshtml
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
