ee34dee41dc9aa9227d1eeb8ce4e1bc66e8a5a43
Yosendra <yosendra.evri@gmail.com>
2024-03-19 16:55:09 +0800

    Building Forms - 02. Labels

---
+++ 04. Building Forms/02. Labels/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var customers = _context.Customers
+                .Include(c => c.MembershipType)
+                .ToList();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers
+                .Include(c => c.MembershipType)
+                .SingleOrDefault(x => x.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+    }
+}
