d5b62e681a451a48c1c12836443da9e4bdb8b77a
Yosendra <yosendra.evri@gmail.com>
2024-03-20 05:19:20 +0800

    Implementing Validation - 01. Adding Validation

---
+++ 05. Implementing Validation/01. Adding Validation/Vidly/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* Adding Validation
+ * 
+ * Constraint a property through attribute (data annotation)
+ * In Customer, we use several attribute for constraint a few property
+ * In CustomersController.Save() we try to get access to validation parameter result through ModelState property
+ * If customer not valid, return the same form along with displaying validation message in the form
+ * 
+ * Add validation message in the form "CustomerForm" page
+ * 
+ * Look at:
+ * • Customer.cs
+ * • CustomersController.cs -> Save()
+ * • CustomerForm.cshtml
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ 05. Implementing Validation/01. Adding Validation/Vidly/ViewModels/CustomerFormVM.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class CustomerFormVM
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
