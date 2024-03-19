105e5766cb9618cfb8a95dbf42872a8e79568470
Yosendra <yosendra.evri@gmail.com>
2024-03-20 06:28:27 +0800

    Implementing Validation - 04. Custom Validation

---
+++ 05. Implementing Validation/04. Custom Validation/Vidly/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* Custom Validation
+ * 
+ * We are going to create our custom data annotation
+ * The rule is user must be over than 18 years to be a member beside "Pay as You Go" category 
+ * 
+ * Create a custom annotation class "Min18YearsIfAMember" under Models folder
+ * In Customer class, use our custom annotation class
+ * 
+ * Add validation message in CustomerForm for BirthDate input
+ * 
+ * Look at:
+ * • Min18YearsIfAMember.cs
+ * • Customer.cs
+ * • CustomerForm.cshtml
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ 05. Implementing Validation/04. Custom Validation/Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute  // this is a must to inherit this class 
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            // first check the selected membership type, that's the role of validationContext
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1) // this is for "Pay as You Go" category
+                return ValidationResult.Success;
+
+            if (customer.BirthDate == null)
+                return new ValidationResult("Birthdate is required");
+
+            var age = DateTime.Today.Year - customer.BirthDate.Value.Year;
+            return age >= 18 
+                ? ValidationResult.Success 
+                : new ValidationResult("Customer should be at least 18 years old to go on a membership");
+
+        }
+    }
+}
