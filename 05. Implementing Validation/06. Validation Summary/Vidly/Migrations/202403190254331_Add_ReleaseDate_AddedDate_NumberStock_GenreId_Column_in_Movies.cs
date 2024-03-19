ae058c6a26fd95772568520401c2190f751f839d
Yosendra <yosendra.evri@gmail.com>
2024-03-20 07:45:07 +0800

    Implementing Validation - 06. Validation Summary

---
+++ 05. Implementing Validation/06. Validation Summary/Vidly/Migrations/202403190254331_Add_ReleaseDate_AddedDate_NumberStock_GenreId_Column_in_Movies.cs
@@ -0,0 +1,30 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_ReleaseDate_AddedDate_NumberStock_GenreId_Column_in_Movies : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "ReleaseDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Movies", "AddedDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Movies", "NumberInStock", c => c.Int(nullable: false));
+            AddColumn("dbo.Movies", "GenreId", c => c.Int(nullable: false));
+            AlterColumn("dbo.Movies", "Name", c => c.String(nullable: false));
+            CreateIndex("dbo.Movies", "GenreId");
+            AddForeignKey("dbo.Movies", "GenreId", "dbo.Genres", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Movies", "GenreId", "dbo.Genres");
+            DropIndex("dbo.Movies", new[] { "GenreId" });
+            AlterColumn("dbo.Movies", "Name", c => c.String());
+            DropColumn("dbo.Movies", "GenreId");
+            DropColumn("dbo.Movies", "NumberInStock");
+            DropColumn("dbo.Movies", "AddedDate");
+            DropColumn("dbo.Movies", "ReleaseDate");
+        }
+    }
+}
