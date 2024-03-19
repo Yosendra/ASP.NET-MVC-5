b97c38524f720b1ca5edb28fdd8c28ba1944c8cf
Yosendra <yosendra.evri@gmail.com>
2024-03-19 19:18:11 +0800

    Building Forms - 06. Edit Form

---
+++ 04. Building Forms/06. Edit Form/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,82 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var customers = _context.Customers
+                .Include(c => c.MembershipType)
+                .ToList();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers
+                .Include(c => c.MembershipType)
+                .SingleOrDefault(x => x.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        public ActionResult New()
+        {
+            var membershiptypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormVM
+            {
+                MembershipTypes = membershiptypes,
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(CustomerFormVM model)
+        {
+            // saving the added customer to the database
+            _context.Customers.Add(model.Customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormVM
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList(),
+            };
+
+            /* We are going to use the existing form which is New.cshtml
+             * It is possible to use it through here by passing these arguments
+             * in this View() method invocation
+            */
+            return View("CustomerForm", viewModel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ 04. Building Forms/06. Edit Form/Vidly/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* Edit Form
+ * 
+ * We are going to create Edit Form for customer
+ * Add action method Edit() in CustomerController
+ * 
+ * Rename NewCustomerVM into CustomerFormVM
+ * Rename New.cshtml into CustomerForm.cshtml
+ * 
+ * Notice in the New() action method, we pass CustomerForm in the View()
+ * because we have changing New.cshtml into CustomerForm.cshtml
+ * 
+ * We change the url of Customer hyperlink to direct to the Customer Form
+ * 
+ * Look at:
+ * • CustomersController -> Edit(), Index.cshtml
+ * • CustomerFormVM.cs
+ * • CustomerForm.cshtml
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
