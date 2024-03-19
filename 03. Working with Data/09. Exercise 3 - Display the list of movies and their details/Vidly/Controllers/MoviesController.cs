6872793ce8c97fb749f896deeb594aad39636c9a
Yosendra <yosendra.evri@gmail.com>
2024-03-19 15:49:48 +0800

    Working with Data - 09. Exercise 3: Display the list of movies and their details

---
+++ 03. Working with Data/09. Exercise 3 - Display the list of movies and their details/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            //var movies = GetMovies();
+            var movies = _context.Movies
+                .Include(m => m.Genre)      // eager loading
+                .ToList();
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            //var movies = GetMovies();
+
+            //var movie = movies.SingleOrDefault(x => x.Id == id);
+            var movie = _context.Movies
+                .Include(m => m.Genre)      // eager loading
+                .SingleOrDefault(x => x.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        // Not needed anymore, we use the data in database
+        //private IEnumerable<Movie> GetMovies()
+        //{
+        //    return new List<Movie>
+        //    {
+        //        new Movie { Id = 1, Name = "Shrek"},
+        //        new Movie { Id = 2, Name = "Wall-e"},
+        //    };
+        //}
+    }
+}
---
+++ 03. Working with Data/09. Exercise 3 - Display the list of movies and their details/Vidly/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* Exercise 3 - Display the list of movies and their details
+ * 1. Add Genres table
+ * 2. Populate Genre table using migration
+ * 3. Add column ReleaseDate, AddedDate, Stock, GenresId in Movie
+ * 
+ * Look at:
+ * • Genre.cs
+ * • ApplicationDbContext.cs
+ * • 202403190224271_Add_Genres_table.cs
+ * • 202403190226115_Populate_Genres_Table.cs
+ * • Movie.cs
+ * • 202403190254331_Add_ReleaseDate_AddedDate_NumberStock_GenreId_Column_in_Movies.cs
+ * • MoviesController.cs
+ * • Movies -> Index.cshtml, Details.cshtml
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
