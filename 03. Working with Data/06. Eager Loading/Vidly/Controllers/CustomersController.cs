9a34ea743763457af58cbee1f8ae3f4391076faf
Yosendra <yosendra.evri@gmail.com>
2024-03-19 09:17:15 +0800

    Working with Data - 06. Eager Loading

---
+++ 03. Working with Data/06. Eager Loading/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            //var customers = _context.Customers.ToList();
+
+            /* Eager Loading by using .Include()
+             * Need using "System.Data.Entity" namespace
+            */
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(x => x.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ 03. Working with Data/06. Eager Loading/Vidly/Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* Eager Loading
+ * 
+ * In Customer, currently we are displaying name only, what if we want to have another column DiscountRate available for each customer
+ * Add to display DiscountRate column in customer table
+ * 
+ * Test: / (exception happen)
+ * 
+ * This caused by null reference of "customer.MembershipType.DiscountRate"
+ * To solve this, we need to load the customers along with their MembershipType together.
+ * This is called eager loading
+ * 
+ * Do eager loading for customers in CustomersController
+ * 
+ * Test: / (normal)
+ * 
+ * Look at:
+ * • Customer -> Index.cshtml
+ * • CustomersController.cs
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
