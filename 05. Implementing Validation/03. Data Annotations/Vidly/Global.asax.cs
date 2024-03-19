850b384a48b314b1195ea4747edf49b0cc00cfe5
Yosendra <yosendra.evri@gmail.com>
2024-03-20 05:57:32 +0800

    Implementing Validation - 03. Data Annotations

---
+++ 05. Implementing Validation/03. Data Annotations/Vidly/Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* Data Annotations
+ * • [Required]
+ * • [StringLength(255)]
+ * • [Range(1, 10)]
+ * • [Compare("OtherProperty")]
+ * • [Phone]
+ * • [EmailAddress]
+ * • [Url]
+ * • [RegularExpression("...")]
+ * • etc
+ * 
+ * These are built-in data annotation by ASP.NET Web Framework
+ * They are have their own default validation message, but we can override it
+ * 
+ * We override the error message for [Required] attribute in Name property of Customer
+ * 
+ * Look at:
+ * • Customer.cs
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
