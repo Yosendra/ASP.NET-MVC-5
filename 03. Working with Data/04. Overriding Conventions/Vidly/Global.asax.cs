88c6e5504c3d4e18e63d2dfd1846e5303f8ceaa9
Yosendra <yosendra.evri@gmail.com>
2024-03-19 08:41:30 +0800

    Working with Data - 04. Overriding Conventions

---
+++ 03. Working with Data/04. Overriding Conventions/Vidly/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* Overriding Conventions
+ * 
+ * We want to change the type of Name column in Customers table.
+ * It is nvarchar(max) and nullable, we want to change it not-nullable and max length to 255
+ * using data annotation approach.
+ * (Another method is using FluentAPI)
+ * 
+ * Give constraint attribute for Name property in Customer class
+ *  Add-Migration ApplyAnnotationToCustomerName
+ *  Update-Database
+ * 
+ * Now observe the Name column in Customer table
+ * 
+ * Look at:
+ * • Customer.cs
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ 03. Working with Data/04. Overriding Conventions/Vidly/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public byte MembershipTypeId { get; set; }
+        public MembershipType MembershipType { get; set; }  
+    }
+}
