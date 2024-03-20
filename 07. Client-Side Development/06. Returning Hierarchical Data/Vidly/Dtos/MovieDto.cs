622bdb86263758e1a9ea0229c7809c038a3ce72b
Yosendra <yosendra.evri@gmail.com>
2024-03-21 03:58:51 +0800

    Client-Side Development - 06. Returning Hierarchical Data

---
+++ 07. Client-Side Development/06. Returning Hierarchical Data/Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime AddedDate { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+    }
+}
---
+++ 07. Client-Side Development/06. Returning Hierarchical Data/Vidly/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.App_Start;
+
+/* Returning Hierarchical Data
+ * 
+ * Create MembershipTypeDto
+ * Add new MembershipTypeDto type property in CustomerDto
+ * 
+ * MappingProfile, configure how to mapping this MembershipTypeDto in CustomerDto
+ * 
+ * In CustomersController.Index() we need to eager-load customer along with MembershipType
+ * 
+ * Look at:
+ * • MembershipTypeDto.cs
+ * • CustomerDto.cs
+ * • MappingProfile.cs
+ * • CustomersController.cs
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
