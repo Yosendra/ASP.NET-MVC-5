2b47867f86fee4dc224cbf1413b42371e3ef4553
Yosendra <yosendra.evri@gmail.com>
2024-03-20 21:09:08 +0800

    Building RESTful Services with ASP.NET Web API - 04. Auto Mapper

---
+++ 06. Building RESTful Services with ASP.NET Web API/04. Auto Mapper/Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/customers
+        public IEnumerable<CustomerDto> GetCustomers()
+        {
+            return _context.Customers.ToList()
+                .Select(Mapper.Map<Customer, CustomerDto>);
+        }
+
+        // GET /api/customers/1
+        public CustomerDto GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            else
+                return Mapper.Map<Customer,CustomerDto>(customer);
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public CustomerDto CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            customerDto.Id = customer.Id;
+
+            return customerDto;
+        }
+
+        // PUT /api/customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var existingCustomer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (existingCustomer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                Mapper.Map<CustomerDto, Customer>(customerDto, existingCustomer);
+
+                //existingCustomer.Name = customerDto.Name;
+                //existingCustomer.BirthDate = customerDto.BirthDate;
+                //existingCustomer.IsSubscribedToNewsletter = customerDto.IsSubscribedToNewsletter;
+                //existingCustomer.MembershipTypeId = customerDto.MembershipTypeId;
+                _context.SaveChanges();
+            }
+        }
+
+        // DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var existingCustomer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (existingCustomer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                _context.Customers.Remove(existingCustomer);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ 06. Building RESTful Services with ASP.NET Web API/04. Auto Mapper/Vidly/Global.asax.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.App_Start;
+
+/* Auto Mapper
+ * 
+ * Install package AutoMapper (4.1)
+ * Make sure the same version
+ * 
+ * After installing we need to create mapping profile to determine
+ * how object of different types can be mapped to each other
+ * 
+ * Under App_Start folder, create class "MappingProfile"
+ * Configure the mapping profile
+ * 
+ * In Global.asax.cs, initialize the automapper
+ * 
+ * Now in CustomersController, we can change every type Customer into CustomerDto
+ * 
+ * Look at:
+ * • MappingProfile.cs
+ * • Global.asax.cs
+ * • CustomersController.cs
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());     // initialize the automapper
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
