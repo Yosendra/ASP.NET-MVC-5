9a98d94a80b35e392e25564b682a09e435aed0a4
Yosendra <yosendra.evri@gmail.com>
2024-03-20 20:04:50 +0800

    Building RESTful Services with ASP.NET Web API - 01. Building an API

---
+++ 06. Building RESTful Services with ASP.NET Web API/01. Building an API/Vidly/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+
+/* Building an API
+ * 
+ * Create new folder named "Api" under Controllers folder.
+ * Right click on it, add new controller for web api
+ * Install the necessary packages in Nuget Package Manager
+ * Read readme.txt which appears after generating api controller
+ * 
+ * Look at:
+ * • WebApiConfig.cs
+ * • Global.asax.cs
+ * • CustomersController.cs (API)
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
