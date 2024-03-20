75678389fbdb63fa01a013d547f8c9ee558970d7
Yosendra <yosendra.evri@gmail.com>
2024-03-20 18:54:32 +0800

    Implementing Validation - 09. Exercise

---
+++ 05. Implementing Validation/09. Exercise/Vidly/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* Exercise
+ * 
+ * Do the same validation now in MovieForm.cshtml
+ * 
+ * Look at:
+ * • MovieFormVM.cs
+ * • MovieForm.cshtml
+ * • MoviesController.cs
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
