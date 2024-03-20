81c1ec3903a486c4338af68263ee0a82d4a3fc09
Yosendra <yosendra.evri@gmail.com>
2024-03-21 01:53:15 +0800

    Client-Side Development - 02. Bootbox Plug-in

---
+++ 07. Client-Side Development/02. Bootbox Plug-in/Vidly/Global.asax.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.App_Start;
+
+/* Bootbox Plug-in
+ * 
+ * Need to install package "bootbox" (4.3.0) in Nuget Package Manager
+ * We get bootbox.js and bootbox.min.js
+ * Register bootbox.js to the bundle in BundleConfig
+ * 
+ * In Index.cshtml, we replace the implementation of confirm() dialog box with bootbox
+ * 
+ * Look at:
+ * • BundleConfig.cs
+ * • Customers -> index.cshtml
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());     // initialize the automapper
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
