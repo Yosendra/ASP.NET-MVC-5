a60fd79fb9193908efd76ef782c1e0eac48217a7
Yosendra <yosendra.evri@gmail.com>
2024-03-20 21:19:06 +0800

    Building RESTful Services with ASP.NET Web API - 05. Using Camel Notation

---
+++ 06. Building RESTful Services with ASP.NET Web API/05. Using Camel Notation/Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
---
+++ 06. Building RESTful Services with ASP.NET Web API/05. Using Camel Notation/Vidly/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.App_Start;
+
+/* Using Camel Notation
+ * 
+ * Before, the return json from Web API is styled in PascalCase.
+ * We want it to presented in camelCase since it is a javscript syntax.
+ * We can configure it in WebApiConfig.cs
+ * 
+ * Run, go to the endpoint for querying customer in Web API
+ * 
+ * Look at:
+ * • WebApiConfig.cs
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());     // initialize the automapper
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
