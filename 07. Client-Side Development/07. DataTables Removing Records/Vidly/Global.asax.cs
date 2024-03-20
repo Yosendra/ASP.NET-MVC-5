5bc544962666d8c74975e9415de8cebb34485a1f
Yosendra <yosendra.evri@gmail.com>
2024-03-21 06:05:05 +0800

    Client-Side Development - 07. DataTables Removing Records

---
+++ 07. Client-Side Development/07. DataTables Removing Records/Vidly/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.App_Start;
+
+/* DataTables Removing Records
+ * 
+ * Before there is a bug. When we delete a customer, then search the deleted customer.
+ * We can still find it in the table.
+ * To fix this we need to invoke API draw() of datatables.
+ * Look at the implementation at Index.cshtml (Customers)
+ * 
+ * Look at:
+ * • Customers -> Index.cshtml
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
