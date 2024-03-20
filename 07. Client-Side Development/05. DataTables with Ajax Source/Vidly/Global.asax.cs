9714a45b5543b4d43eb18fcb2a2aab984fa8cc78
Yosendra <yosendra.evri@gmail.com>
2024-03-21 03:34:19 +0800

    Client-Side Development - 05. DataTables with Ajax Source

---
+++ 07. Client-Side Development/05. DataTables with Ajax Source/Vidly/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.App_Start;
+
+/* DataTables with Ajax Source
+ * 
+ * To use our API as the source of data, we need to configure it at $("#customers").DataTable();
+ * 
+ * Look at:
+ * • Customers -> Index.cshtml
+ * • CustomersController.cs -> Index()
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
