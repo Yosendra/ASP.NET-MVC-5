2d32886b9919c042672210eeca6dc60fed29ee7b
Yosendra <yosendra.evri@gmail.com>
2024-03-21 00:48:43 +0800

    Building RESTful Services with ASP.NET Web API - 07. Exercise

---
+++ 06. Building RESTful Services with ASP.NET Web API/07. Exercise/Vidly/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.App_Start;
+
+/* Exercise
+ * 
+ * We are going to create Web API for Movies
+ * 
+ * Create MovieDto under Dtos folder
+ * Register new profile for Movie in MappingProfile for AutoMapper
+ * Implmenting endpoint for CRUD operation for Movies Web API
+ * 
+ * Look at:
+ * • MovieDto.cs
+ * • MappingProfile.cs
+ * • MoviesController.cs (API)
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());     // initialize the automapper
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ 06. Building RESTful Services with ASP.NET Web API/07. Exercise/Vidly/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
