f4f5b1d0f612a66a8716a6fd89bbd6549118785a
Yosendra <yosendra.evri@gmail.com>
2024-03-21 01:31:53 +0800

    Client-Side Development - 01. Calling an API Using jQuery

---
+++ 07. Client-Side Development/01. Calling an API Using jQuery/Vidly/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.App_Start;
+
+/* Calling an API Using jQuery
+ * 
+ * We are going to consume API for deleting customer
+ * Add button delete button to trigger delete customer in index.cshtml
+ * Add scripts for the logic for manipulating DOM and consuming API through AJAX
+ * 
+ * Look at:
+ * • Customers -> index.cshtml
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());     // initialize the automapper
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
