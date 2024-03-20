aa1d0e24816eab578766c96d63c91740e73cf760
Yosendra <yosendra.evri@gmail.com>
2024-03-21 02:57:27 +0800

    Client-Side Development - 04. DataTables Plug-in

---
+++ 07. Client-Side Development/04. DataTables Plug-in/Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Entity to Dto
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+
+            // Dto to Entity
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(m => m.Id, option => option.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(m => m.Id, option => option.Ignore());
+        }
+    }
+}
