d313d784838e37d3de8d49635d4d0c1b086dc87b
Yosendra <yosendra.evri@gmail.com>
2024-03-21 02:04:26 +0800

    Client-Side Development - 03. Optimizing jQuery Code

---
+++ 07. Client-Side Development/03. Optimizing jQuery Code/Vidly/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.App_Start;
+
+/* Optimizing jQuery Code
+ * 
+ * At Index.cshtml (Customer) at this syntax
+ *      $("#customers .js-delete").on("click", function () {
+ *      
+ * If there are 20 customers, each customer will have delete button, for each button we will have a seperate handler
+ * button in memory. The more customer, the memory is consumed.
+ * We change the selector become like this
+ *      $("#customers").on("click", ".js-delete", function () {
+ *      
+ * The hook will be only one (table#customer), then it will filter element which has class .js-delete, then execute the callback
+ * 
+ * Look at:
+ * • Customers -> Index.cshtml
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());     // initialize the automapper
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ 07. Client-Side Development/03. Optimizing jQuery Code/Vidly/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime AddedDate { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        [Display(Name = "Stock")]
+        public int NumberInStock { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public int GenreId { get; set; }
+
+        public Genre Genre { get; set; }
+    }
+}
