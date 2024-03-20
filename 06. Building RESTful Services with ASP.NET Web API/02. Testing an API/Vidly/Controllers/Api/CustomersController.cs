4b920f08ec7e0636ffb2012ee2ddfa1997bd1712
Yosendra <yosendra.evri@gmail.com>
2024-03-20 20:31:17 +0800

    Building RESTful Services with ASP.NET Web API - 02. Testing an API

---
+++ 06. Building RESTful Services with ASP.NET Web API/02. Testing an API/Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // GET /api/customers/1
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            else
+                return customer;
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        // PUT /api/customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var existingCustomer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (existingCustomer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                existingCustomer.Name = customer.Name;
+                existingCustomer.BirthDate = customer.BirthDate;
+                existingCustomer.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+                existingCustomer.MembershipTypeId = customer.MembershipTypeId;
+                _context.SaveChanges();
+            }
+        }
+
+        // DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var existingCustomer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (existingCustomer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                _context.Customers.Remove(existingCustomer);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ 06. Building RESTful Services with ASP.NET Web API/02. Testing an API/Vidly/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+
+/* Testing an API
+ * 
+ * Testing the API through Postman
+ * Run the application, in postman test the endpoints that we have in our API
+ * 
+ * GET  http://localhost:59744/api/customers
+ * GET  http://localhost:59744/api/customers/1
+ * POST http://localhost:59744/api/customers
+ * 
+ * Look at:
+ * • CustomersController.cs (API)
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
