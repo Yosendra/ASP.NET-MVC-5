cbec5a05a140719d1787913ebd428a12b3edfff0
Yosendra <yosendra.evri@gmail.com>
2024-03-20 08:04:30 +0800

    Implementing Validation - 07. Client-side Validation

---
+++ 05. Implementing Validation/07. Client-side Validation/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,97 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var customers = _context.Customers
+                .Include(c => c.MembershipType)
+                .ToList();
+            return View(customers);
+        }
+        
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers
+                .Include(c => c.MembershipType)
+                .SingleOrDefault(x => x.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        public ActionResult New()
+        {
+            var membershiptypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormVM
+            {
+                Customer = new Customer(),
+                MembershipTypes = membershiptypes,
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormVM
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList(),
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(CustomerFormVM model)
+        {
+            // Get access to the validation result of "CustomerFormVM" which contains Customer class
+            if (!ModelState.IsValid)
+            {
+                // If the customer is not valid, return back to the form
+                model.MembershipTypes = _context.MembershipTypes.ToList();
+                return View("CustomerForm", model);
+            }
+
+            if (model.Customer.Id == 0)
+            {
+                _context.Customers.Add(model.Customer);
+            }
+            else
+            {
+                var existingCustomer = _context.Customers.Single(c => c.Id == model.Customer.Id);
+                existingCustomer.Name = model.Customer.Name;
+                existingCustomer.BirthDate = model.Customer.BirthDate;
+                existingCustomer.MembershipTypeId = model.Customer.MembershipTypeId;
+                existingCustomer.IsSubscribedToNewsletter = model.Customer.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ 05. Implementing Validation/07. Client-side Validation/Vidly/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* Client-side Validation
+ * 
+ * We are going to enable client-side validation by using jquery-val bundle
+ * Add a section script at most bottom of CustomerForm.cshtml
+ * 
+ * Notice: client-side validation only works on built-in data annotation.
+ *         for our custom data annotation like "Min18YearsIfAMember",
+ *         it only works on Server side, not supported in client-side.
+ *         If you want, it needs some jquery code
+ * 
+ * Look at:
+ * • CustomerForm.cshtml
+ * • _Layout.cshtml
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
