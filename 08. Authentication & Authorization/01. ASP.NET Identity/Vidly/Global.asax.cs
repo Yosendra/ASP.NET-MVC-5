1801ac280cf50de06a3e8b5d66a2b847085db784
Yosendra <yosendra.evri@gmail.com>
2024-03-21 17:44:42 +0800

    Authentication & Authorization - 01. ASP.NET Identity

---
+++ 08. Authentication & Authorization/01. ASP.NET Identity/Vidly/Global.asax.cs
@@ -0,0 +1,27 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+using Vidly.App_Start;
+
+/* We are migrating to ASP.NET Identity project scaffold.
+ * A lot of copy-paste files happen from previous solution
+ * we have created.
+*/
+
+namespace Vidly
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
